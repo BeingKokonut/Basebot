91315226728601a6c41aae69d4780a88d079d34b
itsbased <71930306+BeingKokonut@users.noreply.github.com>
2022-04-19 19:03:12 +0400

    .

---
+++ functions/events/discord/reaction_roles/remove_role.js
@@ -0,0 +1,14 @@
+const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN})
+
+if(context.params.event.channel_id == 947509762042495006)
+{
+  if (context.params.event.emoji.name == "📢")
+{
+  await lib.discord.guilds['@0.2.3'].members.roles.destroy
+  ({
+    role_id: `944692049276436500`,
+    user_id: `${context.params.event.member.user_id}`,
+    guild_id: `${context.params.event.guild_id}`
+  });
+}
+}
